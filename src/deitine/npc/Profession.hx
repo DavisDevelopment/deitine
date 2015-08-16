b6fa5ad4d8bac351661452c785a980a852071b4e
Ryan Davis <epikit@gmail.com>
2015-08-15 23:55:42 -0500

    Rewrote the deitine.npc.Profession enum

--- src/deitine/npc/Profession.hx
+++ src/deitine/npc/Profession.hx
@@ -1,9 +1,31 @@
 package deitine.npc;
 
 enum Profession {
-	None;
+	/**
+	  * Increases the 'faith' output of other Citizens by 1
+	  * Allows Player to interact with the Resource system
+	  */
 	Priest;
+
+	/**
+	  * (if Village has at least one Priest)
+	    Can exchange Resources for Gold
+	  */
+	Merchant;
+
+	/**
+	  * GETS: food, fur
+	  * USES: nothing
+	  */
 	Hunter;
+	
+	/**
+	  * GETS: wood
+	  * USES: nothing
+	  */
 	Woodcutter;
-	Carpenter;
+
+	/**
+	  * Allows 
+	  */
 }
