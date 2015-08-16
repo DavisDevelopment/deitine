2f543426ad8c2af619ebee13b3dbe58876cfd0c1
Ryan Davis <epikit@gmail.com>
2015-08-15 23:56:36 -0500

    Created the deitine.core.Player class

---
+++ src/deitine/core/Player.hx
@@ -0,0 +1,14 @@
+package deitine.core;
+
+import deitine.core.Engine;
+
+class Player {
+	/* Constructor Function */
+	public function new():Void {
+		faith = 0;
+	}
+
+/* === Instance Fields === */
+
+	public var faith : Int;
+}
