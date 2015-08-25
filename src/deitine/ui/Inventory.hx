0d49d86ebce4a0ed1fa8105307ae840b48e776ee
Ryan Davis <epikit@gmail.com>
2015-08-25 17:37:32 -0500

    Created the ui/Inventory Component

---
+++ src/deitine/ui/Inventory.hx
@@ -0,0 +1,52 @@
+package deitine.ui;
+
+import deitine.ui.KVTable;
+import deitine.core.Component;
+import deitine.core.Entity;
+import deitine.core.Player;
+import deitine.npc.Village;
+
+class Inventory extends Component {
+	/* Constructor Function */
+	public function new(e : Entity):Void {
+		super( e );
+		player = cast e;
+		table = new KVTable();
+		table.appendTo('body');
+		table.activate();
+
+		table.addRow('followers');
+		table.addRow('faith');
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Method invoked daily
+	  */
+	override public function day():Void {
+		super.day();
+		showInventory();
+	}
+
+	/**
+	  * Update [this] Component
+	  */
+	override public function update():Void {
+		super.update();
+		showInventory();
+	}
+
+	/**
+	  * Do things with the Widget
+	  */
+	private function showInventory():Void {
+		table.write(player.inventory);
+	}
+
+/* === Instance Fields === */
+
+	private var village : Village;
+	private var player : Player;
+	private var table : KVTable;
+}
