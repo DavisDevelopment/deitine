25a7ea3ef27b393aa4714ad147f11df939997658
Ryan Davis <epikit@gmail.com>
2015-08-28 00:43:36 -0500

    Updated the deitine.ui.Inventory widget

--- src/deitine/ui/Inventory.hx
+++ src/deitine/ui/Inventory.hx
@@ -6,13 +6,16 @@ import deitine.core.Entity;
 import deitine.core.Player;
 import deitine.npc.Village;
 
+using deitine.ds.Formatting;
+using StringTools;
+
 class Inventory extends Component {
 	/* Constructor Function */
 	public function new(e : Entity):Void {
 		super( e );
 		player = cast e;
 		table = new KVTable();
-		table.appendTo('body');
+		table.heading.text = 'Inventory';
 		table.activate();
 	}
 
@@ -31,6 +34,7 @@ class Inventory extends Component {
 	  */
 	override public function update():Void {
 		super.update();
+		village = player.village;
 		showInventory();
 	}
 
@@ -38,12 +42,23 @@ class Inventory extends Component {
 	  * Do things with the Widget
 	  */
 	private function showInventory():Void {
-		table.write(player.inventory);
+		var inv = player.inv;
+		
+		/* The Total Population of the Village */
+		table.set('followers', village.population.prettyInt());
+
+		/* The Total Faith Points the Player has */
+		table.set('faith', inv.faith.prettyInt());
+
+		/* The Supplies the Village Has */
+		table.set('food', inv.meat.prettyInt());
+		table.set('wood', inv.wood.prettyInt());
+		table.set('leather', inv.leather.prettyInt());
 	}
 
 /* === Instance Fields === */
 
 	private var village : Village;
 	private var player : Player;
-	private var table : KVTable;
+	public var table : KVTable;
 }
