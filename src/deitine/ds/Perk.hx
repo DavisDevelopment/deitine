b667739037b0aae80e0a72593c2e6f751aec5a96
Ryan Davis <epikit@gmail.com>
2015-08-28 02:25:17 -0500

    Created the deitine.ds.Perk base-class

---
+++ src/deitine/ds/Perk.hx
@@ -0,0 +1,13 @@
+package deitine.ds;
+
+class Perk {
+	/* Constructor Function */
+	public function new():Void {
+		
+	}
+
+/* === Instance Fields === */
+
+	public var name : String;
+	public var description : String;
+}
