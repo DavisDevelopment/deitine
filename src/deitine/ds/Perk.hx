8773df0fe1677658e8435c2c8c4a6b993d721bcf
Ryan Davis <epikit@gmail.com>
2015-09-11 19:39:59 -0500

    Finished the deitine.ds.Perk class

--- src/deitine/ds/Perk.hx
+++ src/deitine/ds/Perk.hx
@@ -3,11 +3,25 @@ package deitine.ds;
 class Perk {
 	/* Constructor Function */
 	public function new():Void {
-		
+		max_level = 80;
+		level = 1;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Invoke the effects caused by [this] Perk
+	  */
+	public function affect():Void {
+		trace('We\'re sorry, but this is not implemented');
 	}
 
 /* === Instance Fields === */
 
 	public var name : String;
 	public var description : String;
+
+	private var lelveled : Bool;
+	private var max_level : Int;
+	private var level : Int;
 }
